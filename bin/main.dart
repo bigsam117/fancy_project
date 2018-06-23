d143ce253fa18e49942c896fce06f3297377d753
Sami Hassen <bigsam117@gmail.com>
2018-06-24 01:01:14 +0200

    start bike class

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,27 @@
-import 'package:fancy_project/fancy_project.dart' as fancy_project;
+class Bicycle{
+  int speed;
+  int gear;
+  int cadence;
+ 
+  // construct
+  Bicycle(this.speed, this.gear, this.cadence);
 
-main(List<String> arguments) {
-  print('Hello world: ${fancy_project.calculate()}!');
+  // message override
+  @override
+  String toString() => "Bicycle: $speed kmh";
+  
+  // function for brake the car's speed
+  void brake(int decrement) {
+    speed -= decrement;
+  }
+  
+  // function for acceleration the car's speed
+  void speedUp(int increment) {
+    speed += increment;
+  }
+        
+}
+void main() {
+  var bike = new Bicycle(60, 20, 0);
+  print(bike);  
 }
--- test/fancy_project_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:fancy_project/fancy_project.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
